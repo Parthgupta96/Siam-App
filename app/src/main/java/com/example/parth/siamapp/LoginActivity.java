9be72f9d6f500327d9b09a40c3bd59b62d1bc46a
Parth Gupta <Parth Gupta>
2017-01-05 21:29:39 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/parth/siamapp/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.parth.siamapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+    // TODO: 05-01-2017 Add FireBase Login
+    Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        init();
+    }
+
+    private void init() {
+        login = (Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                supportFinishAfterTransition(); 
+                startActivity(intent);
+            }
+        });
+    }
+}
