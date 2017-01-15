bd3e53285280cf7829089dcc27acbcac6a37d2d2
Priyanshu Goyal <Priyanshu Goyal>
2017-01-15 13:23:59 +0530

    get current time from firebase server

--- app/src/main/java/com/example/parth/siamapp/ContestFragment.java
+++ app/src/main/java/com/example/parth/siamapp/ContestFragment.java
@@ -1,7 +1,5 @@
 package com.example.parth.siamapp;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,55 +7,41 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ContestFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ContestFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ContestFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     RecyclerView mContestRecyclerView;
     ContestRecyclerViewAdapter mAdapter;
     List<QuestionObject> mQuestions;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mQuestionsDatabaseRef;
+    private DatabaseReference mUsersDatabaseRef;
+    private UserObject mCurrentUser;
+    private Long mTimeStamp;
+    private SimpleDateFormat mDateFormat;
+    private Date mCurrentDate;
 
     public ContestFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContestFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContestFragment newInstance(String param1, String param2) {
+    public static ContestFragment newInstance(UserObject currentUser) {
         ContestFragment fragment = new ContestFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("currentUser", currentUser);
+        fragment.setArguments(bundle);
         return fragment;
     }
 
@@ -65,8 +49,7 @@ public class ContestFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mCurrentUser = (UserObject) getArguments().getSerializable("currentUser");
         }
     }
 
@@ -77,9 +60,14 @@ public class ContestFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_contest, container, false);
 
         mContestRecyclerView = (RecyclerView)root.findViewById(R.id.contest_questions_recycler_view);
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mQuestionsDatabaseRef = mFirebaseDatabase.getReference().child("Questions");
+        mUsersDatabaseRef = mFirebaseDatabase.getReference().child("Users").child(mCurrentUser.getEmail().replace('.', ','));
+
         QuestionObject obj1 = new QuestionObject();
 //        obj1.name = "abc";
         mQuestions = new ArrayList<>();
+        GetCurrentTimeFromFirebase();
         mQuestions.add(obj1);
         mQuestions.add(obj1);
 
@@ -89,6 +77,25 @@ public class ContestFragment extends Fragment {
         mAdapter.notifyDataSetChanged();
 
         return root;
+    }
+
+    private void GetCurrentTimeFromFirebase() {
+        mUsersDatabaseRef.child("Time").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mTimeStamp = (Long)dataSnapshot.child("Time").getValue();
+                mDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                mCurrentDate = new Date(mTimeStamp);
+                mCurrentDate = mDateFormat.format(new Date(mTimeStamp));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mUsersDatabaseRef.child("Time").setValue(ServerValue.TIMESTAMP);
+
     }
 
 }
